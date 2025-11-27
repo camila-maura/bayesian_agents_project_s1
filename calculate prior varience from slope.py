2ae0ef8b18099f3039a8253661ebdd76740e12a5
Rowan Kearney <107808659+Pogaldock@users.noreply.github.com>
2025-11-27 16:03:24 +0100

    Refactor calculate_prior_variance function to return sqrt

--- calculate prior varience from slope.py
+++ calculate prior varience from slope.py
@@ -1,15 +1,14 @@
 
 from math import sqrt
 
+def calculate_prior_variance(sigma_d: float, sigma_s1: float, sigma_s2: float, noise_std: float = 0.35) -> float:
 
-def calculate_prior_variance(sigma_d: float, sigma_s1: float, sigma_s2: float) -> float:
-
-    S = sigma_d ** 2
-    S1 = 1 / (sigma_s1 ** 2)
-    S2 = 1 / (sigma_s2 ** 2)
+    S = sigma_d**2
+    S1 = 1 / ((sigma_s1 + noise_std) ** 2)
+    S2 = 1 / ((sigma_s2 + noise_std) ** 2)
 
     prior_variance = (2 * S) / (2 - (S * (S1 + S2)) + (sqrt((S ** 2) * ((S1 - S2) ** 2) + 4)))
 
-    return 1 / prior_variance
+    return sqrt(prior_variance)
 
 print(calculate_prior_variance(sigma_d=5.9, sigma_s1=2, sigma_s2=8))
